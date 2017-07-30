37d5be3238a27fe5db891563f5162cd47ba211c7
Matt Dupree <kmdupr33@gmail.com>
2017-07-30 11:11:24 -0400

    add node scrap code and server file

---
+++ node-scrap-code/whatever.js
@@ -0,0 +1,17 @@
+[
+  { type: TYPE_QUERY_STRING, payload: '' },
+  { type: TYPE_QUERY_STRING, payload: 'F' },
+  { type: TYPE_QUERY_STRING, payload: 'Fr' },
+  { type: TYPE_QUERY_STRING, payload: 'Fra' },
+  { type: TYPE_QUERY_STRING, payload: 'Fran' },
+  { type: TYPE_QUERY_STRING, payload: 'Franc' },
+  { type: TYPE_QUERY_STRING, payload: 'France' }
+]
+
+Map
+
+[[resultOfExecutingTheQuery], [], []]
+
+mergeAll
+
+[resultOfExecutingTheQuery, ..., ..., ...]
